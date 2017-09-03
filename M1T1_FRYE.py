1d228e86c104952cf9e79552887c48ba536d78e1
fryej7125 <31580288+fryej7125@users.noreply.github.com>
2017-09-02 22:49:45 -0400

    Add files via upload
    first submission... hope this suffices

---
+++ M1T1_FRYE.py
@@ -0,0 +1,6 @@
+Python 3.4.3 (v3.4.3:9b73f1c3e601, Feb 24 2015, 22:43:06) [MSC v.1600 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print("hello world")
+hello world
+>>> #i didnt really understand what you were asking for in that comment part so here you go
+>>> 
